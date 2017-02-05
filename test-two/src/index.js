d2e8fa54aa988e4c84f1b4bce66e43e72cf9480e
Avram Colak <avrcolak@gmail.com>
2017-02-06 06:10:52 +1100

    Made the OAuth token external to the program and the repository

--- test-two/src/index.js
+++ test-two/src/index.js
@@ -1,18 +1,18 @@
 import Logger from './helpers/logger';
 import GitHubService from './services/github';
 import Http from './helpers/http';
+import fs from 'fs';
 
 const baseUrl = "https://api.github.com/";
 const organisationId = "uber";
-const gitHubApiAuthToken = "f98b5f75bfaac8b025ede6e4599b856f6df81a60";
+const gitHubApiAuthToken = fs.readFileSync("./.OAuth", 'utf8');
 
 let logger = new Logger();
 let http = new Http();
-
 let gitHubService = new GitHubService(baseUrl, http, gitHubApiAuthToken);
 
 gitHubService.getUsersForOrganisation(organisationId).then((users) => {
-    users.forEach((user) => {        
+    users.forEach((user) => {
         gitHubService.getRepositoriesForUser(user.login).then((repositories) => {
             logger.log("Username: " + user.login);
             logger.log("Repositories:");
